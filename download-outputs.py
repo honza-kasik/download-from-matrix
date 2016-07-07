ccc06b1c7bbdaf532698af159f23d7d2c3f2943c
Jan Kasik <jkasik@redhat.com>
2016-07-07 11:58:06 +0200

    Initial commit

--- download-outputs.py
+++ download-outputs.py
@@ -5,36 +5,47 @@ import getpass
 import re
 import os
 
-def isolate_matrix_configuration_from_url(url):
+def isolate_matrix_configuration_from_url(url, matrix_scopes):
     """
-    Isolates data from url, e.g.: 'https://eap-qe-jenkins.rhev-ci-vms.eng.rdu2.redhat.com/job/richfaces-4.5-metamer-smoke-ftest-eap70x-matrix-jkasik/SUITE=core,TEMPLATE=aRepeat,jdk=oracle-java-1.8,label=RHEL&&medium/25/testReport/api/python'
     :param url: url to process
-    :return: string containg "TEMPLATE-SUITE"
+    :return: string which contains "SCOPE1-SCOPE2...."
     """
-    template = re.search('TEMPLATE=(.+?)[^a-zA-Z0-9]', url).group()
-    suite = re.search('SUITE=(.+?)[^a-zA-Z0-9]', url).group()
-    return template + '-' + suite
+    name = ''
+    for scope in matrix_scopes:
+        if name == '':
+            name += re.search(scope + '=(.+?)[^a-zA-Z0-9]', url).group(1)
+        else:
+            name += '-' + re.search(scope + '=(.+?)[^a-zA-Z0-9]', url).group(1)
+    return name
+    # template = re.search('TEMPLATE=(.+?)[^a-zA-Z0-9]', url).group(1)
+    # suite = re.search('SUITE=(.+?)[^a-zA-Z0-9]', url).group(1)
+    # return template + '-' + suite
 
 def check_if_folder_exists(folder_name):
     if not os.path.isdir(folder_name):
         os.mkdir(folder_name)
 
-def download_console_output_for_each_configuration(runs, build_number):
+def download_console_output_for_each_configuration(runs, build_number, matrix_scopes):
     check_if_folder_exists(build_number)
     for run in runs:
         console = run.get_console()
-        f = open(build_number + os.path.sep + isolate_matrix_configuration_from_url(run.get_result_url()), 'w')
+        path = build_number + os.path.sep + isolate_matrix_configuration_from_url(run.get_result_url(), matrix_scopes)
+        print "Saving file to " + path
+        f = open(path, 'w')
         f.write(console)
         f.close()
 
 def main(argv):
     print argv
-    use_string = argv[0] + " -b <build-number> -u <username> -p <password>"
+    use_string = argv[0] + " -b <number> -u <username> [-p <password>] -m <scope1,scope2...> -s <url> -j <job-name>"
     build_number = ''
     username = ''
     password = ''
+    server_url = ''
+    job_name = ''
+    matrix_scopes = []
     try:
-        opts, args = getopt.getopt(argv, "hb:u:p:",["build=", "username=", "password="])
+        opts, args = getopt.getopt(argv[1:], "hb:u:p:m:s:j:",["build=", "username=", "password=", "matrix-scopes=", "server-url=", "job-name="])
     except getopt.GetoptError:
         print use_string
         sys.exit(2)
@@ -48,25 +59,25 @@ def main(argv):
             username = arg
         elif opt in ("-p", "--password"):
             password = arg
+        elif opt in ("-m", "--matrix-scopes"):
+            matrix_scopes = arg.split(',')
+        elif opt in ("-s", "--server-url"):
+            server_url = arg
+        elif opt in ("-j", "--job-name"):
+            job_name = arg
 
-    if username == '':
-        print("Username is requested! \nUsage: " + use_string)
-        sys.exit(2)
-
-    if build_number == '':
-        print("Build number is requested! \nUsage: " + use_string)
-        sys.exit(2)
+    # todo requested arguments!!
 
     if password == '':
         password = getpass.getpass()
 
-    server = Jenkins('https://eap-qe-jenkins.rhev-ci-vms.eng.rdu2.redhat.com/', ssl_verify=False, username=username,
+    server = Jenkins(server_url, ssl_verify=False, username=username,
                      password=password)
-    master_job = server.get_job('richfaces-4.5-metamer-smoke-ftest-eap70x-matrix-jkasik')
+    master_job = server.get_job(job_name)
     build = master_job.get_build(int(build_number))
     runs = build.get_matrix_runs()
 
-    download_console_output_for_each_configuration(runs=runs, build_number=build_number)
+    download_console_output_for_each_configuration(runs=runs, build_number=build_number, matrix_scopes=matrix_scopes)
 
 if __name__ == "__main__":
    main(sys.argv)
